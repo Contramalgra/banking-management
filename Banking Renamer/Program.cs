1192da8f89185347fbc657b9ad688b2a47cf35e7
Stephen Laboucane <slabouca@sfu.ca>
2019-01-01 14:38:57 -0800

    Add project files.

---
+++ Banking Renamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banking_Renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
