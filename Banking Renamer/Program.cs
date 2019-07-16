c21c1c328df29370829fedb7ff32e9e5e56b3213
Stephen Laboucane <starwars_laboucane@yahoo.ca>
2019-07-16 16:02:47 -0700

    Replace hardcoded calls to RenameBankFiles with stored objects
    Need to embed bool ShowDay better

--- Banking Renamer/Program.cs
+++ Banking Renamer/Program.cs
@@ -1,20 +1,11 @@
 using System;
 using System.Globalization;
+using Banking_Renamer.Banks;
 
 namespace Banking_Renamer
 {
     class Program
     {
-        private const string SearchString_Scotia = "e-Statement";
-        private const string SearchString_Tangerine = "Tangerine-Chequing_";
-        private const string SearchString_CoastCapital = @"statement-\d+-";
-        private const string SearchString_BMO = "eStatement_";
-        private const string SearchString_RBC = @"[\dX]+-\w{8,9}-";
-        private const string SearchString_Brim = @"statement-\w{32}-";
-        //private const string SearchString_CIBC = @"CIBC\";
-
-        //public const string RootBankingFolder = "Banking";
-
         private static void PrintHeader()
         {
             Console.WriteLine(@" _____ _ _        ____                                      ");
@@ -32,33 +23,13 @@ namespace Banking_Renamer
 
             var rf = new Renamer(rootDirectory);
 
-            rf.RenameBankFiles(SearchString_Scotia);
-
-            var anonymousFunction = new Func<string, DateTime>(s => DateTime.ParseExact(s, "MMMyy", CultureInfo.CurrentCulture));
-            rf.RenameBankFiles(SearchString_Tangerine, anonymousFunction);
-
-            rf.RenameBankFiles(
-                SearchString_CoastCapital,
-                "*.pdf",
-                s => DateTime.ParseExact(s, "yyMMMdd", CultureInfo.CurrentCulture)
-            );
-
-            rf.RenameBankFiles(SearchString_BMO, s => DateTime.ParseExact(s, "yyyy-MM-dd", CultureInfo.CurrentCulture), true);
-
-            // rf.RenameBankFiles(SearchString_RBC, @"RBC\*.pdf", DateTime.Parse, true);
-
-            rf.RenameBankFiles(
-                SearchString_Brim,
-                @"Brim\statement-*.pdf",
-                s => DateTime.ParseExact(s, "yyyyMMdd", CultureInfo.CurrentCulture),
-                true
-            );
-
-            rf.RenameBankFiles(
-                "", //@"^[^-]*$",
-                @"CIBC\*.pdf",
-                s => DateTime.Parse(s)
-            );
+            rf.RenameBankFiles(new Scotia());
+            rf.RenameBankFiles(new Tangerine());
+            rf.RenameBankFiles(new CoastCapitalSavings());            
+            rf.RenameBankFiles(new BMO(), true);
+            rf.RenameBankFiles(new RBC(), true);
+            rf.RenameBankFiles(new BRIM(), true);
+            rf.RenameBankFiles(new GenericBank("CIBC"));
         }
     }
 }
